b48ad0455256215bf1e10ec9a79271041e64d853
lozye <lozye@qq.com>
2020-06-21 13:44:46 +0800

    v20200621

--- doc/centosshadowsockslibev.sh
+++ doc/centosshadowsockslibev.sh
@@ -2,25 +2,28 @@
 ## shadowsocks-libev
 ######################
 
+# yum update
+yum update
+
 # install dependencies
 yum install epel-release -y
-yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel -y
+yum install nano gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel -y
 
 # install shadowsocks-libev
 cd /etc/yum.repos.d/
 wget https://copr.fedoraproject.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo
-yum update
-yum install shadowsocks-libev
+yum install shadowsocks-libev -y
 
 # edit config.json
-vi /etc/shadowsocks-libev/config.json
+nano /etc/shadowsocks-libev/config.json
 {
     "server":"0.0.0.0",
     "server_port":8888,
     "local_port":1080,
     "password":"xxxxxxxxxxx",
     "timeout":60,
-    "method":"aes-256-cfb"
+    "method":"aes-256-cfb",
+    "fast_open":true,
 }
 
 # run application on startup
@@ -57,4 +60,23 @@ open https://github.com/xtaci/kcptun/releases
 
 # run on startup
 launchctl load run_client.plist
+
+# bbr https://teddysun.com/489.html
+cd /home
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+# wait for reboot
+
+sysctl net.ipv4.tcp_available_congestion_control
+# show net.ipv4.tcp_available_congestion_control = bbr cubic reno || net.ipv4.tcp_available_congestion_control = reno cubic bbr
+
+lsmod | grep bbr
+# show bbr info
+
+# install
+yum --enablerepo=elrepo-kernel -y install kernel-ml-headers
+
+# if error yum remove kernel-headers
+yum --enablerepo=elrepo-kernel -y install kernel-ml-headers
+
 
